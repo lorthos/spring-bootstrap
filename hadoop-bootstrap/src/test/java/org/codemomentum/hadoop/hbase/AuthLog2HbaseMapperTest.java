c2b64183519d525e5aa307f7868db1dab4805f7a
codemomentum <codemomentum@gmail.com>
2012-01-19 13:05:41 +0200

    hbase job

--- hadoop-bootstrap/src/test/java/org/codemomentum/hadoop/hbase/AuthLog2HbaseMapperTest.java
+++ hadoop-bootstrap/src/test/java/org/codemomentum/hadoop/hbase/AuthLog2HbaseMapperTest.java
@@ -1,31 +1,48 @@
 package org.codemomentum.hadoop.hbase;
 
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mrunit.mapreduce.MapDriver;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 /**
  * @author codemomentum@gmail.com
  */
 public class AuthLog2HbaseMapperTest {
+	AuthLog2HbaseMapper mapper;
+	Mapper.Context mockContext;
 
-	private MapDriver driver;
 
 	@Before
 	public void setUp() {
-		AuthLog2HbaseMapper mapper = new AuthLog2HbaseMapper();
-		driver = new MapDriver(mapper);
+		mapper = new AuthLog2HbaseMapper();
+		mockContext = mock(Mapper.Context.class);
 	}
 
 	@Test
-	public void testBasicFlow() {
-		driver.withInput(new LongWritable(1l), new Text("Dec 18 11:08:33 ip-11-111-111-111 sshd[6893]: Invalid user handan from 118.219.234.227"))
-				.withInput(new LongWritable(2l), new Text("Dec 18 11:08:48 ip-11-111-111-111 sshd[6909]: Invalid user field from 118.219.234.227"))
-				.withInput(new LongWritable(3l), new Text("Dec 19 21:41:24 ip-11-111-111-111 sshd[21212]: Invalid user admin from 123.49.55.132"))
-				.withInput(new LongWritable(4l), new Text("Dec 19 21:43:21 ip-11-111-111-111 sshd[21300]: Invalid user admin from 123.49.55.132"))
-				.withInput(new LongWritable(4l), new Text("Dec 19 21:43:21 ip-11-111-111-111 sshd[21302]: Invalid user mysql from 123.49.55.133"))
-				.runTest();
+	public void testBasicFlow() throws Exception {
+		mapper.map(new LongWritable(1l), new Text("Dec 18 11:08:33 ip-11-111-111-111 sshd[6893]: Invalid user oracle from 118.219.234.227"), mockContext);
+
+		ArgumentCaptor<Put> argument = ArgumentCaptor.forClass(Put.class);
+		verify(mockContext).write(anyObject(), argument.capture());
+		assertTrue("Should contain the message as content:message",
+				argument.getValue().has(Bytes.toBytes("content"), Bytes.toBytes("message"), Bytes.toBytes("Invalid user oracle from 118.219.234.227")));
+
+
+		verifyNoMoreInteractions(mockContext);
+
 	}
 }
